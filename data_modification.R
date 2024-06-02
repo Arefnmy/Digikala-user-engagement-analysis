fc341bffe49278f6019078df03b1204aedb73ec6
Nima <nima.helli5@gmail.com>
2024-06-02 16:37:52 +0330

    Update data_modification.R
    - Replaced values in the advantages and disadvantages columns with their respective counts of items.
    
    - Replaced "nan" strings with 0 in the true_to_size_rate column and other values with 1.
    
    - Converted "recommendation_status" values to numeric representations for "recommended" (1), "not_recommended" (1), "no_idea" (0.5), and others (0).
    
    - Replacing existing titles with 1 and "nan" values with 0.
    
    - Replaced the body of each comment with the word count.
    
    SUMMARY: Data transformation and feature engineering steps were taken to prepare datasets for further analysis.

--- data_modification.R
+++ data_modification.R
@@ -1,5 +1,7 @@
 # Function to count items in the advantages and disadvantages columns
 count_items <- function(x) {
+  # Handle NA or "nan" values
+  if (is.na(x) || x == "nan") return(0)
   # Remove leading and trailing square brackets and split the string by comma
   items <- strsplit(gsub("\\[|\\]", "", x), ",\\s*")[[1]]
   # Return the count of items
@@ -7,15 +9,50 @@ count_items <- function(x) {
 }
 
 # Apply the function to the advantages and disadvantages columns
-comments_modified_data <- comments_modified_data %>%
+new_comments<- new_comments %>%
   mutate(
     advantages_count = sapply(advantages, count_items),
     disadvantages_count = sapply(disadvantages, count_items)
   )
 
 # Drop the original advantages and disadvantages columns (if desired)
-comments_modified_data <- comments_modified_data %>%
+new_comments <- new_comments %>%
   select(-advantages, -disadvantages)
 
+#####################################################
+
+# Function to count words in a string
+count_words <- function(text) {
+  # Split the text by spaces and count the number of elements
+  str_count <- length(unlist(strsplit(text, "\\s+")))
+  return(str_count)
+}
+
+# Apply the function to the body column and
+# replace the original text with the word count
+new_comments <- new_comments %>%
+  mutate(body = sapply(body, count_words))
+
+#####################################################
+
+# Replace the values in the recommendation_status column
+new_comments <- new_comments %>%
+  mutate(recommendation_status = case_when(
+    recommendation_status %in% c("recommended", "not_recommended") ~ 1,
+    recommendation_status == "no_idea" ~ 0.5,
+    TRUE ~ 0
+  ))
+
+#####################################################
+
+# Replace the values in the title column
+new_comments <- new_comments %>%
+  mutate(title = ifelse(is.na(title) | title == "nan" | title == "", 0, 1))
+
+#####################################################
+
+# Replace "nan" with 0 and other values with 1 in 'true_to_size_rate' column
+new_comments <- new_comments %>%
+  mutate(true_to_size_rate = ifelse(true_to_size_rate == "nan", 0, 1))
 
 #####################################################
